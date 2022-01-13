6f59f0d6e103b046cefe3c82345f9d833d74369a
Damodar Das <dinesh.k.kaushish@gmail.com>
2022-01-13 21:35:49 +0000

    Adds Database Configuration

---
+++ src/main/java/com/vanamadhuryam/calendarscraper/config/DatabaseConfig.java
@@ -0,0 +1,48 @@
+package com.vanamadhuryam.calendarscraper.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix="datasource")
+public class DatabaseConfig {
+
+    private String driverName;
+    private String url;
+    private String username;
+    private String password;
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
